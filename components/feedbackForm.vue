e4dcdea32da3e5780c65fcc2f3c1437e7e35f327
Tara Z. Manicsic <tzmanics@gmail.com>
2022-02-19 18:02:25 -0500

    adds feedback form

---
+++ components/feedbackForm.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <form
+      netlify
+      action="/"
+      method="POST"
+      name="feedback"
+    >
+      <input type="hidden" name="form-name" value="feedback" />
+      <input type="text" name="name" />
+      <input type="email" placeholder="email" required>
+      <input type="string" placeholder="feedback" required>
+    </form>
+  </div>
+</template>
