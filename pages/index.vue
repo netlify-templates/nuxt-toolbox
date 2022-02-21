24059f8e756e0b477b5755bbb5dfdea18c3fd3fa
Tara Z. Manicsic <tzmanics@gmail.com>
2022-02-21 17:26:40 -0500

    merges deploy stuff and adds form component

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <template>
-  <Tutorial />
+  <feedback-form />
 </template>
 
 <script>
+import FeedbackForm from '../components/FeedbackForm.vue';
 export default {
   name: 'IndexPage',
+  components: { FeedbackForm },
 }
 </script>
