9ca2f85ed31f3ad9d377a7667b7faad74894709b
Ben Hong <ben@bencodezen.io>
2022-02-23 10:41:15 -0800

    fix: add form name

--- components/FeedbackForm.vue
+++ components/FeedbackForm.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="feedback-form">
-    <form netlify netlify-honeypot method="post" action="/success">
+    <form
+      netlify
+      netlify-honeypot
+      name="feedback"
+      method="POST"
+      action="/success"
+    >
       <p class="hidden">
         <label>
           Don’t fill this out if you’re human: <input name="bot-field" />
