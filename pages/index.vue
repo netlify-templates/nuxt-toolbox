60cc9e7298cd9a61fa8f1f1852e4353648feb280
Prince Wilson <maxcell.wilson@gmail.com>
2022-04-21 11:14:11 -0400

    fix: Addressing typo

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div class="intro">
       <p>Hi 👋! This template gives you a <a href="https://nuxtjs.org/">Nuxt</a> app with the scaffolding for <a href="https://www.netlify.com/products/functions/">Netlify Functions</a>, <a href="https://www.netlify.com/products/forms/">Forms</a>, and <a href="https://docs.netlify.com/routing/redirects/">Redirects</a>. Our aim was to give you the code you would need to hit the ground running with a few fun features.</p>
 
-      <p>You can find the code for this project on GtiHub at <a href="https://github.com/netlify-templates/nuxt-toolbox">https://github.com/netlify-templates/nuxt-toolbox</a>! Happy coding!</p>
+      <p>You can find the code for this project on GitHub at <a href="https://github.com/netlify-templates/nuxt-toolbox">https://github.com/netlify-templates/nuxt-toolbox</a>! Happy coding!</p>
     </div>
     <FeedbackForm />
     <JokeBlock />
